186061a3f6020e9a7604eb3e732d3b42d9697a54
李振宇 <879681805@qq.com>
2018-10-13 17:05:45 +0800

    lzy test

--- src/main/java/com/uporanges/controller/teacher/TeacherController.java
+++ src/main/java/com/uporanges/controller/teacher/TeacherController.java
@@ -1,13 +1,5 @@
 package com.uporanges.controller.teacher;
 
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Proxy;
-
-import com.uporanges.entity.t_company;
 
 public class TeacherController {
-   private ClassLoader loader;
-private Class<?>[] interfaces;
-private InvocationHandler h;
-t_company tt = Proxy.newProxyInstance(loader, interfaces, h);
 }
