706521e5913bb00fe16c44a2494ed89e595fbb5c
BattleCall <1131527000@qq.com>
2018-10-16 18:12:49 +0800

    保存bc修改

--- src/main/java/com/uporanges/controller/deal/StudentController.java
+++ src/main/java/com/uporanges/controller/deal/StudentController.java
@@ -32,8 +32,27 @@ public class StudentController {
 	private StudentService studentService;
 	
 	/*
-	 * 增加学生信息
-	 * 学生修改个人信息
+	 * bc 10.16
+	 * 注册时判断手机号或邮箱是否注册过
+	 */
+	@GetMapping("/ifPhoneIn")
+	public void ifPhoneIn(String phone, HttpServletResponse response) {
+		String data = "{\"code\":0}";
+		if(studentService.ifPhoneIn(phone))
+			data = "{\"code\":1}";
+		Util.writeJson(response, data);
+	}
+	@GetMapping("/ifeMailIn")
+	public void ifeMainlIn(String email, HttpServletResponse response) {
+		String data = "{\"code\":0}";
+		if(studentService.ifeMailIn(email))
+			data = "{\"code\":1}";
+		Util.writeJson(response, data);
+	}
+	/*
+	 * bc 10.16
+	 * 增加学生信息,已经是用户
+	 * 增加学生，新用户需要注册新用户
 	 * 这两个方法目前采用requestBody注解，暂时测试不了，如果后期测试不可以的话，用modelAttribute注解是可以的，打开下面的注释。
 	 */
 	@PostMapping("/addStudent")
@@ -49,6 +68,10 @@ public class StudentController {
 	public Map<String, Object> addUserStudent(@RequestBody User user, @RequestBody Student student) {
 		return studentService.addUserStudent(user, student);
 	}
+	/*
+	 * bc 2018.10.15
+	 * 学生修改个人信息
+	 */
 	@PostMapping("/alterStudent")
 	public void alterStudent(@RequestBody Student student, HttpServletResponse response){
 		String data = "{\"code\":200}";
